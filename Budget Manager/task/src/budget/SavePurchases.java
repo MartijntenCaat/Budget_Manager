4d67971c4971ff5a74e5358958c871b9c125ea3c
Martijn ten Caat <martijn.tencaat@edsn.nl>
2020-09-30 20:10:27 +0200

    Did some work, got nowhere.

--- Budget Manager/task/src/budget/SavePurchases.java
+++ Budget Manager/task/src/budget/SavePurchases.java
@@ -1,4 +1,30 @@
 package budget;
 
+import java.io.File;
+import java.io.IOException;
+
 public class SavePurchases {
+    private File file;
+
+    public SavePurchases(File file) {
+        this.file = file;
+    }
+
+    public void savePurchases(PurchaseStore purchaseStore) {
+        file = new File("purchases.txt");
+
+        try {
+            boolean createdNew = file.createNewFile();
+            if (createdNew) {
+                System.out.println("The file was successfully created.");
+            } else {
+                System.out.println("The file already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot create the file: " + file.getPath());
+        }
+
+
+    }
+
 }
